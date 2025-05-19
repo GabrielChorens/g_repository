bac7daf8db9f8aa351be8d7578bc3eec3786ec79
GabrielChorens <65200862+GabrielChorens@users.noreply.github.com>
2025-05-19 08:16:34 -0600

    Enhance "Agregados [Modelado de dominio]" page with additional content and improved styling

---
+++ src/courses.js
@@ -0,0 +1,91 @@
+import course10 from './courses/10-polimorfismo.js';
+// Importar el resto de cursos de manera similar
+
+export const courses = [
+    {
+        title: "01 Qué son los agregados y aggregate roots",
+        videoId: "fk5gHvTOjjU",
+        notes: []
+    },
+    {
+        title: "02 Como se relacionan los agregados",
+        videoId: "tX4c3dGBfmA",
+        notes: []
+    },
+    {
+        title: "03 Problemas de los agregados pequeños",
+        videoId: "9fhUcuNPXGI",
+        notes: []
+    },
+    {
+        title: "04 Utiliza una capa de orquestación para gestionar tus agregados",
+        videoId: "o1MZe58JKPk",
+        notes: []
+    },
+    {
+        title: "05 La importancia de los Bounded Contexts",
+        videoId: "PtHAj4y9pFk",
+        notes: []
+    },
+    {
+        title: "06 CQRS Agregados para escritura vs proyecciones para lectura",
+        videoId: "fOVk_FCnxz4",
+        notes: []
+    },
+    {
+        title: "07 Por qué el identificador lo tiene que generar el frontend",
+        videoId: "4ZTC_EXIPnA",
+        notes: []
+    },
+    {
+        title: "08 Reduce el acoplamiento utilizando un Agregado para cada módulo",
+        videoId: "a_YjkRszD_I",
+        notes: []
+    },
+    {
+        title: "09 Servicios de dominio vs Agregados ¿Dónde van las reglas de negocio",
+        videoId: "hXx9q2CrPkU",
+        notes: []
+    },
+    course10,
+    {
+        title: "11 Validaciones de Agregados cuando cambia el esquema de la BD",
+        videoId: "3AQ1CyO9o90",
+        notes: []
+    },
+    {
+        title: "12 Agregados 100% encapsulados Uso de toPrimitives",
+        videoId: "x88MsTJ20Bs",
+        notes: []
+    },
+    {
+        title: "13 Promoción de módulo a Bounded Context ¿Qué pasa con mis agregados?",
+        videoId: "cWsAsZlKYxw",
+        notes: []
+    },
+    {
+        title: "14 Value Object vs Entidad vs Agregado ¿1 Tabla = 1 Agregado",
+        videoId: "Nv_TrDCetww",
+        notes: []
+    },
+    {
+        title: "15 Cómo gestionar agregados que tienen millones de registros",
+        videoId: "1bb1T5kLYYI",
+        notes: []
+    },
+    {
+        title: "16 Paginación sin ensuciar los Agregados utilizando el patrón Criteria",
+        videoId: "6HJjDl3HiYY",
+        notes: []
+    },
+    {
+        title: "17 Cómo testear con Agregados si dentro tienen eventos de dominio",
+        videoId: "S8c7cRJw4BQ",
+        notes: []
+    },
+    {
+        title: "18 Cómo implementar código DDD usando Inteligencia Artificial",
+        videoId: "vJFEIntw-tI",
+        notes: []
+    }
+]; 
